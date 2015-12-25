01cdb3c2823aebc56ec9ddd6961b40a93a44c8ba
shashank <shashank.sharma98@gmail.com>
2015-12-25 14:00:25 +0530

    Python 3 program

---
+++ hey1.py
@@ -0,0 +1,10 @@
+print ("Welcome")
+
+age = input("Please enter your age : ")
+print (age)
+if age < 18:
+  print ("You are a Child")
+else:
+  print ("You are an adult")
+
+print ("Thank you for participating")
