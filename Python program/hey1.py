29c0f60b0526869649b46d7e2c00c86751028c9f
shashank <shashank.sharma98@gmail.com>
2016-01-06 19:29:50 +0530

    New look to home page

---
+++ Python program/hey1.py
@@ -0,0 +1,10 @@
+print ("Welcome")
+
+age = int(input("Please enter your age : "))
+print (age)
+if age < 18:
+  print ("You are a Child")
+else:
+  print ("You are an adult")
+
+print ("Thank you for participating")
